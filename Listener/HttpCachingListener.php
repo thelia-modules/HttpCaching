cbdaa07f146dc787d751b81fe94e8ef669d63b21
Manuel Raynaud <raynaud.manu@gmail.com>
2014-03-05 16:09:52 +0100

    init HttpCaching module

---
+++ Listener/HttpCachingListener.php
@@ -0,0 +1,83 @@
+<?php
+/*************************************************************************************/
+/*                                                                                   */
+/*      Thelia	                                                                     */
+/*                                                                                   */
+/*      Copyright (c) OpenStudio                                                     */
+/*      email : info@thelia.net                                                      */
+/*      web : http://www.thelia.net                                                  */
+/*                                                                                   */
+/*      This program is free software; you can redistribute it and/or modify         */
+/*      it under the terms of the GNU General Public License as published by         */
+/*      the Free Software Foundation; either version 3 of the License                */
+/*                                                                                   */
+/*      This program is distributed in the hope that it will be useful,              */
+/*      but WITHOUT ANY WARRANTY; without even the implied warranty of               */
+/*      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                */
+/*      GNU General Public License for more details.                                 */
+/*                                                                                   */
+/*      You should have received a copy of the GNU General Public License            */
+/*	    along with this program. If not, see <http://www.gnu.org/licenses/>.         */
+/*                                                                                   */
+/*************************************************************************************/
+
+namespace HttpCaching\Listener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+
+/**
+ * Class HttpCachingListener
+ * @package HttpCaching\Listener
+ * @author manuel raynaud <mraynaud@openstudio.fr>
+ */
+class HttpCachingListener implements EventSubscriberInterface
+{
+
+    public function addCache(FilterResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        $path = $request->getPathInfo();
+        if (!preg_match("#^(/admin/)([^/]+)#i", $path))
+        {
+            if (false === $request->attributes->has('no_cache')) {
+                $response = $event->getResponse();
+                $response->setMaxAge($request->attributes->get('max-age', false) ?: 600);
+                $response->setSharedMaxAge($request->attributes->get('s-maxage', false) ?: 600);
+
+                $event->setResponse($response);
+            }
+        }
+
+    }
+
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * The array keys are event names and the value can be:
+     *
+     *  * The method name to call (priority defaults to 0)
+     *  * An array composed of the method name to call and the priority
+     *  * An array of arrays composed of the method names to call and respective
+     *    priorities, or 0 if unset
+     *
+     * For instance:
+     *
+     *  * array('eventName' => 'methodName')
+     *  * array('eventName' => array('methodName', $priority))
+     *  * array('eventName' => array(array('methodName1', $priority), array('methodName2'))
+     *
+     * @return array The event names to listen to
+     *
+     * @api
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            KernelEvents::RESPONSE => array('addCache', 128)
+        );
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,5 @@
+#HttpCaching
+
+This module allow you to define which route can be cached and a specific expiration time.
+
+You can only use expiration cache. Validation cache must be set in each controller you want to use.
